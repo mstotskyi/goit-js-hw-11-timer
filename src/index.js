b6cb943a82457654154094c30cb5ef297f1239f8
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-08-15 22:05:26 +0300

    рефакторинг в клас

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
 import './sass/main.scss';
 
-const refs = {
-    secs: document.querySelector('span[data-value="secs"]'),
-    mins: document.querySelector('span[data-value="mins"]'),
-    hours: document.querySelector('span[data-value="hours"]'),
-    days: document.querySelector('span[data-value="days"]'),
 
-};
+class CountdownTimer {
+    constructor({selector, targetDate}) {
+        this.selector = document.querySelector(selector);
+        this.refs = {
+            secs: document.querySelector('span[data-value="secs"]'),
+            mins: document.querySelector('span[data-value="mins"]'),
+            hours: document.querySelector('span[data-value="hours"]'),
+            days: document.querySelector('span[data-value="days"]'),
+        }
+        this.targetDate = targetDate;
+        this.startCountdownTimer();
+    }
 
-let currentDate = null;
-let targetDate = new Date(2021, 10, 12, 0, 0, 0, 0);
-let time = null;
 
-
-
-startCountdownTimer();
-
-function logger () {
-currentDate = Date.now();
-time = targetDate.getTime() - currentDate;
-const sec = Math.floor((time % (1000 * 60)) / 1000);
-const min = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const day = Math.floor((time / (1000 * 60 * 60 * 24)));
-refs.secs.textContent = sec < 10 ? `0${sec}` : sec;
-refs.mins.textContent = min < 10 ? `0${min}` : min;
-refs.hours.textContent = hour < 10 ? `0${hour}` : hour;
-refs.days.textContent = day < 10 ? `0${day}` : day;
-};
-
-function startCountdownTimer () {
-setInterval (logger, 1000);
+    startCountdownTimer () {
+    setInterval (() => {
+        let currentDate = Date.now();
+        let time = this.targetDate.getTime() - currentDate;
+        const sec = Math.floor((time % (1000 * 60)) / 1000);
+        const min = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+        const hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const day = Math.floor((time / (1000 * 60 * 60 * 24)));
+        this.refs.secs.textContent = sec < 10 ? `0${sec}` : sec;
+        this.refs.mins.textContent = min < 10 ? `0${min}` : min;
+        this.refs.hours.textContent = hour < 10 ? `0${hour}` : hour;
+        this.refs.days.textContent = day < 10 ? `0${day}` : day;
+            }, 1000)
+}
 }
 
-
-
-
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date(2021, 10, 12, 0, 0, 0, 0),
+  });
 
