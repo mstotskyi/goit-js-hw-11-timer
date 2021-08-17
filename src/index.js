59536af0cbc483f286dfbb35eda921c5d3fa06fd
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-08-18 00:15:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,26 +14,23 @@ class CountdownTimer {
         this.startCountdownTimer();
     }
 
-
     startCountdownTimer () {
-    this.intervalId = setInterval (() => {
+        this.intervalId = setInterval (() => {
         let currentDate = Date.now();
         let time = this.targetDate.getTime() - currentDate;
         this.renderPage (time);
         this.timeIsUp (time);
-            }, 1000)
+        }, 1000)
     }
     renderPage (time) {
         const sec = Math.floor((time % (1000 * 60)) / 1000);
-            const min = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-            const hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const day = Math.floor(time / (1000 * 60 * 60 * 24));
-            this.refs.secs.textContent = sec < 10 ? `0${sec}` : sec;
-            this.refs.mins.textContent = min < 10 ? `0${min}` : min;
-            this.refs.hours.textContent = hour < 10 ? `0${hour}` : hour;
-            this.refs.days.textContent = day < 10 ? `0${day}` : day;
-            console.log(time);
-            
+        const min = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+        const hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const day = Math.floor(time / (1000 * 60 * 60 * 24));
+        this.refs.secs.textContent = sec < 10 ? `0${sec}` : sec;
+        this.refs.mins.textContent = min < 10 ? `0${min}` : min;
+        this.refs.hours.textContent = hour < 10 ? `0${hour}` : hour;
+        this.refs.days.textContent = day < 10 ? `0${day}` : day;
     }
 
     timeIsUp (time) {
@@ -46,6 +43,6 @@ class CountdownTimer {
 
 new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date(2021, 8, 18, 0, 0, 0, 0),
+    targetDate: new Date(`Oct 12 2021`),
   });
 
